2dbd06484c44190df138d70844047b62a2706198
salboaie <master1!23>
2016-02-15 13:54:51 +0200

    initial adapters for Core services

---
+++ Core/adapters/UDEAdapter.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016 ROMSOFT.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the The MIT License (MIT).
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *    RAFAEL MASTALERU (ROMSOFT)
+ * Initially developed in the context of OPERANDO EU project www.operando.eu
+ */
+
+var core = require("swarmcore");
+thisAdapter = core.createAdapter("UDEAdapter");
+
+getPrivacySettings = function(user_id, osp_list){
+
+}
+
+applyPrivacySettings = function(user_id, ops_id){
+
+}
