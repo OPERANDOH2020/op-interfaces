280ea05e977ebd6b9b1dc6e3fe679285297282dd
salboaie <master1!23>
2016-02-16 19:18:55 +0200

    basic transformation descriptions

--- transformations/st-email.js
+++ transformations/st-email.js
@@ -3,7 +3,26 @@
  * email services
  */
 
-module.exports.st-idm = {
+module.exports.st-email = {
+    name:"SendEmail interface",
+    sendEmail:{
+        method : 'put',
+        params: ["userId", "__to","__cc", "__bcc","__subject", "__content"],
+        path   : '/sendEmail/$userId/',
+        code:function(userId, __to,__cc, __bcc,__subject, __content){
 
+        }
+    },
+    sendEmailWithAttachement:{
+        method : 'delete',
+            params: ["userId", "__to","__cc", "__bcc","__subject", "__content", "__attachments"],
+            path   : '/removeIdentity/$userId/$identity',
+            code:function(userId, __to,__cc, __bcc,__subject, __content, __attachments){
+
+        }
+    }
 
 }
+
+
+
--- transformations/st-idm.js
+++ transformations/st-idm.js
@@ -2,3 +2,31 @@
  * Created by salboaie on 2/15/16.
  * IDM: IDentity Management swarm
  */
+
+module.exports.st_idm = {
+    name: "Identities Manager",
+    generateIdentity:{
+        method : 'put',
+        params: ["userId"],
+        path   : '/addIdentity/$userId/',
+        code:function(){
+
+        }
+    },
+    removeIdentity:{
+        method : 'delete',
+        params: ["userId", "identity"],
+        path   : '/removeIdentity/$userId/$identity',
+        code:function(){
+
+        }
+    },
+    listIdentities:{
+        method : 'get',
+        params: ["userId"],
+        path   : '/listIdentities/$userId/',
+        code:function(){
+
+        }
+    }
+}
