bd7e5e3da64d66891a3b246e96ff6bea01f75250
kostis alexandris <alexandris@cti.gr>
2016-06-13 18:01:28 +0300

    Elimination of weird char at the beginning of the java file

--- op-interfaces-aapi/src/main/java/eu/operando/interfaces/aapi/ticket/controller/TicketController.java
+++ op-interfaces-aapi/src/main/java/eu/operando/interfaces/aapi/ticket/controller/TicketController.java
@@ -1,4 +1,4 @@
-﻿/*******************************************************************************
+/*******************************************************************************
  *  # Copyright (c) 2016 {UPRC}.
  *  # All rights reserved. This program and the accompanying materials
  *  # are made available under the terms of the The MIT License (MIT).
