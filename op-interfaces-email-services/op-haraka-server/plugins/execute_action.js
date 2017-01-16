d122791f01ec37742313d5a1c13a25709925af73
ciprian <cprn_talmacel@yahoo.com>
2017-01-16 13:30:23 +0200

    'from' experiments

--- op-interfaces-email-services/op-haraka-server/plugins/execute_action.js
+++ op-interfaces-email-services/op-haraka-server/plugins/execute_action.js
@@ -65,7 +65,7 @@ exports.forward = function (next, connection) {
             plugin.loginfo("New from: "+newFrom);
             connection.transaction.add_header('From', newFrom);
         }else{
-            var fromMessage = "'"+original+"' <"+newFrom+">";
+            var fromMessage = "'IdentityManager@plusprivacy.com on behalf of "+original+"' <"+newFrom+">";
             plugin.loginfo(fromMessage);
             connection.transaction.add_header('From',fromMessage );
         }
