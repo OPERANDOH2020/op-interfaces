af93696ad6eee23108d42abb8a530ab62e846e8c
kostis alexandris <alexandris@cti.gr>
2017-05-08 17:39:26 +0300

    Another Test in AAPI

--- op-interfaces-aapi/eu.operando.interfaces.aapi.server/src/main/java/eu/operando/interfaces/aapi/user/controller/UserController.java
+++ op-interfaces-aapi/eu.operando.interfaces.aapi.server/src/main/java/eu/operando/interfaces/aapi/user/controller/UserController.java
@@ -242,7 +242,9 @@ public class UserController {
 		Attributes attrs = answer.next().getAttributes();
                 
                 boolean isDeleted = false;
+                names += attrs.get("o")+"_"+attrs.get("cn");
                 if(attrs.get("o")!=null && attrs.get("o").contains("deleted")){
+                    names += "I_am_in_deleted,";
                     isDeleted = true;
                 }
                 if(attrs.get("cn")!=null && !isDeleted)
