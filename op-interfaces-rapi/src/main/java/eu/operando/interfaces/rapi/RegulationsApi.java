OPERANDOH2020/op-interfaces

commit 35e1bf4b79858639746821947962c22168e871a0

OF-543 fix annotations for rapi

op-interfaces-rapi/src/main/java/eu/operando/interfaces/rapi/RegulationsApi.java
@@ -9,44 +9,54 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import eu.operando.api.model.DtoPrivacyRegulation;
 import eu.operando.api.model.PrivacyRegulation;
 import eu.operando.api.model.PrivacyRegulationInput;
 import eu.operando.interfaces.rapi.factories.RegulationsApiServiceFactory;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Path("/regulations")
-
+@RestController
+@RequestMapping(value = "/regulations")
 @Produces({ MediaType.APPLICATION_JSON })
-@io.swagger.annotations.Api(description = "the regulations API")
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-07-12T14:06:26.001Z")
 public class RegulationsApi
 {
 	private final RegulationsApiService delegate = RegulationsApiServiceFactory.getRegulationsApi();
 
 	@POST
 	@Produces({ MediaType.APPLICATION_JSON })
-	@io.swagger.annotations.ApiOperation(
-		value = "Add a new regulation to the system.",
-		notes = "Called by a regulator to ensure that the platform becomes compliant with the new regulation. The regulation that was saved is returned, along with an identifer which should be used to refer to it in later communication. ",
-		response = PrivacyRegulation.class,
-		tags = { "regulations", })
-	@io.swagger.annotations.ApiResponses(
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	@ApiOperation(
+		value = "",
+		notes = "Called by a regulator to ensure that the platform becomes compliant with the new regulation. The regulation that was saved is returned, along with an identifer which should be used to refer to it in later communication.",
+		response = PrivacyRegulation.class)
+	@ApiResponses(
 		value = {
-			@io.swagger.annotations.ApiResponse(
+			@ApiResponse(
 				code = 201,
-				message = "The regulation was successfully created, and the platform has been made compliant with it.",
-				response = PrivacyRegulation.class),
-			@io.swagger.annotations.ApiResponse(
+				message = "The regulation was successfully created, and the platform has been made compliant with it."),
+			@ApiResponse(
 				code = 401,
-				message = "The user is not authenticated with the OPERANDO system. Check that the service ticket provided by the authentication service is correctly included in the message body.",
-				response = PrivacyRegulation.class),
-			@io.swagger.annotations.ApiResponse(
+				message = "The user is not authenticated with the OPERANDO system. Check that the service ticket provided by the authentication service is correctly included in the message body."),
+			@ApiResponse(
 				code = 403,
-				message = "The user is authenticated with the OPERANDO system, but is not allowed to perform the requested action.",
-				response = PrivacyRegulation.class) })
-	public Response regulationsPost(@ApiParam(value = "Ticket proving that the caller is allowed to use this service", required = true) @HeaderParam("service-ticket") String serviceTicket,
-			@ApiParam(value = "", required = true) PrivacyRegulationInput regulation)
+				message = "The user is authenticated with the OPERANDO system, but is not allowed to perform the requested action.") })
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "service-ticket", value = "Ticket proving that the caller is allowed to use this service", required = true, dataType = "string", paramType = "header"),
+		@ApiImplicitParam(name = "regulation", value = "", required = true, dataType = "PrivacyRegulationInput", paramType = "body")
+	})
+	public Response regulationsPost(
+			@ApiIgnore @HeaderParam("service-ticket") String serviceTicket,
+			@ApiIgnore PrivacyRegulationInput regulation)
 	{
 		return delegate.processNewRegulation(serviceTicket, regulation);
 	}
@@ -54,28 +64,31 @@ public class RegulationsApi
 	@PUT
 	@Path("/{reg-id}")
 	@Produces({ MediaType.APPLICATION_JSON })
-	@io.swagger.annotations.ApiOperation(
-		value = "Update an exisiting regulation.",
-		notes = "Called by a regulator to ensure that the platform becomes compliant with the new terms of the regulation. The regulation that was saved is returned, along with an identifer which should be used to refer to it in later communication. ",
-		response = DtoPrivacyRegulation.class,
-		tags = { "regulations" })
-	@io.swagger.annotations.ApiResponses(
+	@RequestMapping(value = "{reg-id}", method = RequestMethod.PUT)
+	@ApiOperation(
+		value = "",
+		notes = "Called by a regulator to ensure that the platform becomes compliant with the new terms of the regulation. The regulation that was saved is returned, along with an identifer which should be used to refer to it in later communication.",
+		response = DtoPrivacyRegulation.class)
+	@ApiResponses(
 		value = {
-			@io.swagger.annotations.ApiResponse(
+			@ApiResponse(
 				code = 204,
-				message = "The regulation was successfully updated, and the platform has been made compliant with it.",
-				response = DtoPrivacyRegulation.class),
-			@io.swagger.annotations.ApiResponse(
+				message = "The regulation was successfully updated, and the platform has been made compliant with it."),
+			@ApiResponse(
 				code = 401,
-				message = "The user is not authenticated with the OPERANDO system. Check that the service ticket provided by the authentication service is correctly included in the message body.",
-				response = DtoPrivacyRegulation.class),
-			@io.swagger.annotations.ApiResponse(
+				message = "The user is not authenticated with the OPERANDO system. Check that the service ticket provided by the authentication service is correctly included in the message body."),
+			@ApiResponse(
 				code = 403,
-				message = "The user is authenticated with the OPERANDO system, but is not allowed to perform the requested action.",
-				response = DtoPrivacyRegulation.class) })
-	public Response regulationsRegIdPut(@ApiParam(value = "Ticket proving that the caller is allowed to use this service", required = true) @HeaderParam("service-ticket") String serviceTicket,
-			@ApiParam(value = "", required = true) PrivacyRegulationInput regulation,
-			@ApiParam(value = "the unique identifier of a regulation.", required = true) @PathParam("reg-id") String regId)
+				message = "The user is authenticated with the OPERANDO system, but is not allowed to perform the requested action.") })
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "service-ticket", value = "Ticket proving that the caller is allowed to use this service", dataType = "string", paramType = "header", required = true),
+		@ApiImplicitParam(name = "regulation", value = "", required = true, dataType = "PrivacyRegulationInput", paramType = "body"), 
+		@ApiImplicitParam(name = "reg-id", value = "The unique identifier of a regulation", required = true, dataType = "string", paramType = "path")  
+	})
+	public Response regulationsRegIdPut(
+			@ApiIgnore @HeaderParam("service-Ticket") String serviceTicket,
+			@ApiIgnore PrivacyRegulationInput regulation,
+			@ApiIgnore @PathParam("reg-Id") String regId)
 	{
 		return delegate.processExistingRegulation(serviceTicket, regulation, regId);
 	}

op-interfaces-rapi/src/main/java/eu/operando/interfaces/rapi/ComplianceReportApi.java
@@ -11,17 +11,26 @@ import javax.ws.rs.core.Response.Status;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import eu.operando.UnableToGetDataException;
 import eu.operando.api.AuthenticationService;
 import eu.operando.api.factories.AuthenticationServiceFactory;
 import eu.operando.api.model.ComplianceReport;
 import eu.operando.interfaces.rapi.factories.ComplianceReportsServiceFactory;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Path("/osps/{osp-id}/compliance-report")
 @Produces({ MediaType.APPLICATION_JSON })
-@io.swagger.annotations.Api(description = "the compliance report API")
+@RestController
+@RequestMapping(value = "/osps/{osp-id}/compliance-report")
 public class ComplianceReportApi
 {
 
@@ -42,22 +51,25 @@ public class ComplianceReportApi
 
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON })
-	@io.swagger.annotations.ApiOperation(value = "Get the compliance report for an OSP.", response = ComplianceReport.class)
-	@io.swagger.annotations.ApiResponses(
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	@ApiOperation(value = "", response = ComplianceReport.class, notes = "Called by a regulator to obtain a compliance report relating to the specified OSP.")
+	@ApiResponses(
 		value = {
-			@io.swagger.annotations.ApiResponse(
+			@ApiResponse(
 				code = 200,
-				message = "The compliance report for the OSP is returned as a JSON object.",
-				response = ComplianceReport.class),
-			@io.swagger.annotations.ApiResponse(
+				message = "The compliance report for the OSP is returned as a JSON object."),
+			@ApiResponse(
 				code = 401,
-				message = "Error - The user is not authenticated with the OPERANDO system. Check that the service ticket provided by the authentication service is correctly included in the message body.",
-				response = ComplianceReport.class),
-			@io.swagger.annotations.ApiResponse(code = 404, message = "Error - The OSP could not be found.", response = ComplianceReport.class),
-			@io.swagger.annotations.ApiResponse(code = 500, message = "Error - An internal error has occured.", response = ComplianceReport.class) })
+				message = "Error - The user is not authenticated with the OPERANDO system. Check that the service ticket provided by the authentication service is correctly included in the message body."),
+			@ApiResponse(code = 404, message = "Error - The OSP could not be found."),
+			@ApiResponse(code = 500, message = "Error - An internal error has occured.") })
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "service-ticket", value = "Ticket proving that the caller is allowed to use this service", required = true, dataType = "string", paramType = "header"),
+		@ApiImplicitParam(name = "osp-id", value = "The unique identifier of an online service provider.", required = true, dataType = "string", paramType = "path")
+	})
 	public Response complianceReportGet(
-			@ApiParam(value = "Ticket proving that the caller is allowed to use this service", required = true) @HeaderParam("service-ticket") String serviceTicket,
-			@ApiParam(value = "The unique identifier of an online service provider.", required = true) @PathParam("osp-id") String ospId)
+		@ApiIgnore @HeaderParam("service-ticket") String serviceTicket,
+		@ApiIgnore @PathParam("osp-id") String ospId)
 	{
 		Response response;
 		try

commit a0f137ff3919c42c4d7ddb59b8935ab60ae82ba8

ALS-453 fix case error introduced in 35e1bf4b79858639746821947962c22168e871a0

op-interfaces-rapi/src/main/java/eu/operando/interfaces/rapi/RegulationsApi.java
@@ -88,7 +88,7 @@ public class RegulationsApi
 	public Response regulationsRegIdPut(
 			@ApiIgnore @HeaderParam("service-Ticket") String serviceTicket,
 			@ApiIgnore PrivacyRegulationInput regulation,
-			@ApiIgnore @PathParam("reg-Id") String regId)
+			@ApiIgnore @PathParam("reg-id") String regId)
 	{
 		return delegate.processExistingRegulation(serviceTicket, regulation, regId);
 	}